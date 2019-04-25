9c9283090d15fcb4e4e6d096829613482731c95d
Edgar Oliveira <aluno19987@ipt.pt>
2019-04-25 11:30:52 +0100

    criação de um formulátio
    adição de uma atividade com um formulário para preencher com os dados necessarios

---
+++ app/src/main/java/com/ipt/expensesocr/Formulario.java
@@ -0,0 +1,52 @@
+package com.ipt.expensesocr;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Formulario extends AppCompatActivity {
+
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        addItemsToSpinner();
+    }
+
+    public void addItemsToSpinner(){
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        List<String> list = new ArrayList<String>();
+        list.add("Transport");
+        list.add("Alojamento");
+        list.add("Refeições");
+        list.add("Despesas Diversas");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+    }
+
+}
