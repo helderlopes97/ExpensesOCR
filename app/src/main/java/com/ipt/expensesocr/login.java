38ea896390bb2a76a93f3f0a0140b59f018724c4
Hélder Lopes <aluno19784@ipt.pt>
2019-05-04 19:50:30 +0100

    funcionamento com apis

--- app/src/main/java/com/ipt/expensesocr/login.java
+++ app/src/main/java/com/ipt/expensesocr/login.java
@@ -2,6 +2,16 @@ package com.ipt.expensesocr;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class login extends AppCompatActivity {
 
@@ -9,6 +19,36 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://my-json-server.typicode.com/helderfoca/ExpensesOCR/users";
+
+        // Request a string response from the provided URL.
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET, url,null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject res) {
+                        // Display the first 500 characters of the response string.
+                        //textView.setText("Response is: "+ response.substring(0,500));
+                        Log.d("Response", "onResponse: Sucesso");
+
+                        Log.d("Result", "onResponse: " + res.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //textView.setText("That didn't work!");
+                        Log.d("Error", "onResponse: Failed");
+                        error.getStackTrace();
+                    }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(req);
+
     }
 
 }
