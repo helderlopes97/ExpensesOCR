9424cf7bc864f58c0e84e08182fa54a35bd8e99f
Edgar Oliveira <aluno19987@ipt.pt>
2019-05-09 13:32:35 +0100

    criação do ecrã das despesa pendentes

---
+++ app/src/main/java/com/ipt/expensesocr/DespesasPendentes.java
@@ -0,0 +1,17 @@
+package com.ipt.expensesocr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DespesasPendentes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_despesas_pendentes);
+    }
+
+    protected void getDespesas(){
+
+    }
+}
