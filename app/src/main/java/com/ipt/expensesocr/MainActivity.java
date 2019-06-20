dd88d40b3cf4ab0706c057273cdcb64cdc2a1031
Hélder Lopes <aluno19784@ipt.pt>
2019-06-20 13:44:28 +0100

    Camara permissions

--- app/src/main/java/com/ipt/expensesocr/MainActivity.java
+++ app/src/main/java/com/ipt/expensesocr/MainActivity.java
@@ -17,6 +17,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -73,17 +74,16 @@ public class MainActivity extends AppCompatActivity {
         } else {
             // Permission has already been granted
         }
-        // Here, thisActivity is the current activity
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},CAMARA);
         } else {
             // Permission has already been granted
         }
-        // Here, thisActivity is the current activity
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},WRITE_EXTERNAL_STORAGE);
         } else {
             // Permission has already been granted
+
         }
 
 
@@ -107,7 +107,12 @@ public class MainActivity extends AppCompatActivity {
         cam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                camera();
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},CAMARA);
+                } else {
+                    // Permission has already been granted
+                    camera();
+                }
             }
         });
 
