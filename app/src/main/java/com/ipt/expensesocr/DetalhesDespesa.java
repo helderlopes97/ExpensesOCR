dec5a22d9c9c2050539431614673da0038a5af4c
Edgar Oliveira <aluno19987@ipt.pt>
2019-06-24 16:17:48 +0100

    Criação do Logout

--- app/src/main/java/com/ipt/expensesocr/DetalhesDespesa.java
+++ app/src/main/java/com/ipt/expensesocr/DetalhesDespesa.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -361,5 +362,26 @@ public class DetalhesDespesa extends AppCompatActivity {
         despesa.addView(textView3);
 
         return despesa;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.topbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                Intent intent = new Intent(DetalhesDespesa.this, Login.class);
+                startActivity(intent);
+                finish();
+                return true;
+        }
+        return false;
+
     }
 }
--- app/src/main/java/com/ipt/expensesocr/Login.java
+++ app/src/main/java/com/ipt/expensesocr/Login.java
@@ -162,4 +162,6 @@ public class Login extends AppCompatActivity {
             }
         });
     }
+
+
 }
